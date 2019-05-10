da019f8ca8e1aca826ce339a465747c20c4939d4
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 21:04:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,4 +4,7 @@ module Findable
 end
 
 
-#
+#This module should define the method, find_by_name. This method will be used as a class method. Something like this:
+
+# Artist.find_by_name("Adele")
+# #=> #<Artistx038230sdcmdn3872>
