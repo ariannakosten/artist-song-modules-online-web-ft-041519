f247a051b76ef31cd2134c9c4dda29b798eba695
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 21:12:36 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,19 +1,8 @@
 module Findable
-   @artists = []
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def all
-    @artists
-  end 
   
   def find_by_name(name)
-    @artists.detect{|a| a.name == name}
-end
+   self.detect{|a| a.name == name}
+  end
 end
 
 
