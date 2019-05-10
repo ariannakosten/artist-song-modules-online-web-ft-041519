34aae552117cf66ed8cd7cca35f68b0bba02bd0f
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 21:41:05 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -12,7 +12,7 @@ module InstanceMethods
   def initialize
     self.class.all << self
     
-    #
+    #.initialize is an instance method. So, inside .initialize, self refers to the instance of the class on which you are operating. But .all is a class method. (include because instance method)
 end
 end
 end
