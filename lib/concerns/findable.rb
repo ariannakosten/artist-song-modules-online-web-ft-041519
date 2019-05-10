149f03bd3bd316ae9889aa2fb32163cb2c483ff8
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 21:05:48 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,7 +4,9 @@ module Findable
 end
 
 
-#This module should define the method, find_by_name. This method will be used as a class method. Something like this:
+#This module should define the method, find_by_name. This method will be used as a class method. 
+#def self.find_by_name --self not needed since keyword extend will tell it is a class method
+#Something like this:
 
 # Artist.find_by_name("Adele")
 # #=> #<Artistx038230sdcmdn3872>
